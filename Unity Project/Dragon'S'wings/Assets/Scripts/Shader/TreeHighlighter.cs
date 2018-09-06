5382f06511f6fc22b23e1b36c8e4f53b59aeb4f6
LordHelmchen12 <freak03@gmx.net>
2018-09-06 12:15:22 +0200

    Level Tut texte, highlight tree Dummie

--- Unity Project/Dragon'S'wings/Assets/Scripts/Shader/TreeHighlighter.cs
+++ Unity Project/Dragon'S'wings/Assets/Scripts/Shader/TreeHighlighter.cs
@@ -8,7 +8,9 @@ public class TreeHighlighter : MonoBehaviour
 
     public GameObject player;
     private GameObject cursor;
+    private Collider2D coll;
     public bool checkTrue = false;
+    private Hook hook;
     // Use this for initialization
     void Start ()
     {
@@ -16,57 +18,42 @@ public class TreeHighlighter : MonoBehaviour
         
         rend.material.SetColor("_Color", new Color(1, 1, 1, 0));
 
-        
+        hook = player.transform.Find("Character").Find("Hook").gameObject.GetComponent<Hook>();
+        Debug.Log(hook);
         cursor = player.transform.Find("Character").Find("Crosshair").gameObject;
         player = player.transform.Find("Character").gameObject;
+
+
+
+        coll = GetComponent<Collider2D>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (checkTrue)
-        {
-            checkTarget();
+      
+            if (checkTarget())
+            {
+                rend.material.SetColor("_Color", new Color(1, 1, 1, 1));
+            }
+
+            else
+            {
+                rend.material.SetColor("_Color", new Color(1, 1, 1, 0));
+            }
             checkTrue = false;
-        }
+        
 	}
 
     public bool checkTarget()
     {
-        Debug.DrawRay(new Vector2(player.transform.position.x, player.transform.position.y),((new Vector2(player.transform.position.x, player.transform.position.y) - (new Vector2(cursor.transform.position.x, cursor.transform.position.y))))* -1000000.0f, Color.white);
-        Debug.Log("Schuss!");
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(player.transform.position.x, player.transform.position.y), ((new Vector2(player.transform.position.x, player.transform.position.y) - (new Vector2(cursor.transform.position.x, cursor.transform.position.y)))), Mathf.Infinity);
-        if (hit.collider != null)
-        { Debug.Log("HIT!"); }
-            return false;
-    }
-
-
-    public bool checkLOS()
-    {
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        //layerMask = ~layerMask;
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(player.transform.position, cursor.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-        {
-            Debug.DrawRay(player.transform.position, cursor.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-        }
-        else
-        {
-            Debug.DrawRay(player.transform.position, cursor.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
-    
-
-
+        Debug.Log(hook.maxRopeLength);
+        RaycastHit2D hit = Physics2D.Raycast(player.transform.position, cursor.transform.position - player.transform.position, hook.maxRopeLength);
+        if (hit.collider == coll)
+            return true;
         return false;
+
     }
+
 
 }
