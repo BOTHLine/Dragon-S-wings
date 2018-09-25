8c7b063d4c6a884a3a0a2122154a27255c4df64f
LordHelmchen12 <freak03@gmx.net>
2018-09-25 17:01:45 +0200

    Demolevel fix

---
+++ Unity Project/Dragon'S'wings/Assets/Scripts/MobileSpriteSorter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileSpriteSorter : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+    // Use this for initialization
+    void Start ()
+    {
+        mySpriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        mySpriteRenderer.sortingOrder = -(int)(this.transform.position.y * 100);
+    }
+}
---
+++ Unity Project/Dragon'S'wings/Assets/Scripts/SortMySpriteLayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortMySpriteLayer : MonoBehaviour
+{
+
+    private SpriteRenderer mySpriteRenderer;
+    
+    // Use this for initialization
+	void Start ()
+    {
+        mySpriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        mySpriteRenderer.sortingOrder = -(int) (this.transform.position.y * 100);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
